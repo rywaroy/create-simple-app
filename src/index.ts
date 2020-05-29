abf168047a20c0a14c01b4df304be7e771d248f8
rywaroy <464485682@qq.com>
2020-05-29 09:01:36 +0800

    feat: 添加在当前目录下创建功能

--- src/index.ts
+++ src/index.ts
@@ -5,21 +5,22 @@ import checkTargetDir from './checkTargetDir';
 const path = require('path');
 const packageJson = require('../package.json');
 
-let projectName: string = '';
+let projectName: string = '.';
 
 const { program } = commander;
 
 program
   .version(packageJson.version)
-  .arguments('<project-directory>')
-  .action((name: string) => {
-    projectName = name;
-  });
+  .arguments('<project-directory>');
 
 program.parse(process.argv);
 
-// 检查合法应用名
-checkAppName(projectName);
+if (program.args.length > 0) { // 创建新文件夹
+  [projectName] = program.args;
+
+  // 检查合法应用名
+  checkAppName(projectName);
+}
 
 const targetDir = path.resolve(process.cwd(), projectName); // 生成项目的目录
 
