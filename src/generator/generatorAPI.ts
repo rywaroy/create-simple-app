9a29b9a520d97258e81592b66fe3e16c5b515ab3
rywaroy <464485682@qq.com>
2020-05-30 21:15:44 +0800

    feat: 添加预设选项方法

--- src/generator/generatorAPI.ts
+++ src/generator/generatorAPI.ts
@@ -1,4 +1,5 @@
 import Generator from './index';
+import { IPrompt } from '../types';
 
 export default class GeneratorAPI {
   id: string;
@@ -8,5 +9,12 @@ export default class GeneratorAPI {
   constructor(id: string, generator: Generator) {
     this.id = id;
     this.generator = generator;
+  }
+
+  /**
+   * 添加预设选项
+   */
+  addPresetPrompt(prompt: IPrompt) {
+    this.generator.presetPrompts.push(prompt);
   }
 }
