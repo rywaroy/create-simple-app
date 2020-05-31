3255ae5278b3cfbc85d4c4326de68c54fbd15d87
rywaroy <464485682@qq.com>
2020-05-31 10:19:27 +0800

    feat: install plugin

---
+++ src/plugins/install.ts
@@ -0,0 +1,19 @@
+import GeneratorAPI from '../generator/generatorAPI';
+
+const installPlugin = {
+  id: 'install',
+  apply: (api: GeneratorAPI) => {
+    api.addPresetPrompt({
+      name: 'type',
+      type: 'list',
+      message: '使用npm/yarn/cpnm',
+      choices: [
+        { name: 'npm', value: 'npm i' },
+        { name: 'yarn', value: 'yarn' },
+        { name: 'cnpm', value: 'cnpm i' },
+      ],
+    });
+  },
+};
+
+export default installPlugin;
