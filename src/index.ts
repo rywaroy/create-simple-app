3b2292ac1884121fd1aaa8ead1e801b2b915e400
rywaroy <464485682@qq.com>
2020-06-01 16:53:57 +0800

    feat: 运行

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import commander from 'commander';
-import checkAppName from './checkAppName';
-import createTargetDir from './createTargetDir';
-import loadPlugins from './loadPlugins';
+import path from 'path';
+import checkAppName from './preset/checkAppName';
+import createTargetDir from './preset/createTargetDir';
+import loadPlugins from './preset/loadPlugins';
+import Generator from './generator';
 
 const packageJson = require('../package.json');
 
@@ -15,16 +17,41 @@ program
 
 program.parse(process.argv);
 
-if (program.args.length > 0) { // 创建新文件夹
-  [projectName] = program.args;
+async function run() {
+  if (program.args.length > 0) { // 创建新文件夹
+    [projectName] = program.args;
 
-  // 检查合法应用名
-  checkAppName(projectName);
+    // 检查合法应用名
+    checkAppName(projectName);
+  }
+
+  const targetDir = path.resolve(process.cwd(), projectName); // 生成项目的目录
+
+  // 创建文件夹
+  await createTargetDir(projectName, targetDir);
+
+  // 遍历加载插件
+  const plugins = loadPlugins();
+
+  const name = projectName === '.' ? path.basename(process.cwd()) : projectName;
+
+  // 生成package.json
+  const pkg = {
+    name,
+    version: '0.1.0',
+    scripts: {},
+    devDependencies: {},
+    dependencies: {},
+  };
+
+  // 初始化Generator类型
+  const generator = new Generator(targetDir, {
+    plugins,
+    pkg,
+  });
+
+  // 调用实例的创建方法
+  generator.create();
 }
 
-// 创建文件夹
-createTargetDir(projectName);
-
-const plugins = loadPlugins();
-
-console.log(plugins);
+run();
