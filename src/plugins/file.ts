d42a077dea2b5c0c91d878ddd826f1d70ff7ee83
rywaroy <464485682@qq.com>
2020-06-04 14:48:00 +0800

    feat: 添加file插件

---
+++ src/plugins/file.ts
@@ -0,0 +1,44 @@
+import GeneratorAPI from '../generator/generatorAPI';
+
+const filePlugin = {
+  id: 'file',
+  apply: (api: GeneratorAPI) => {
+    api.addModulePrompt({
+      name: 'file img/font',
+      value: 'file',
+    });
+    api.addPresetPromptCallBack(({ module }: { module: string[]}) => {
+      // 判断是否选择了file
+      if (module.includes('file')) {
+        api.chainWebpack()
+          .module
+          .rule('image')
+          .test(/\.(png|jpg|gif)$/)
+          .use('url-loader')
+          .loader('url-loader')
+          .options({
+            limit: 8192,
+            name: 'images/[name]_[hash:8].[ext]',
+          });
+        api.chainWebpack()
+          .module
+          .rule('font')
+          .test(/\.(eot|woff2?|ttf|svg)$/)
+          .use('url-loader')
+          .loader('url-loader')
+          .options({
+            limit: 8192,
+            name: 'font/[name]_[hash:8].[ext]',
+          });
+        api.extendPackage({
+          devDependencies: {
+            'url-loader': '^4.1.0',
+            'file-loader': '^6.0.0',
+          },
+        });
+      }
+    });
+  },
+};
+
+export default filePlugin;
