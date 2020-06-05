73685b7116e1c4185e9725933f4a6c8947a8eb4a
rywaroy <464485682@qq.com>
2020-06-05 09:55:07 +0800

    feat: merge函数

--- src/utils/merge.ts
+++ src/utils/merge.ts
@@ -8,14 +8,44 @@ function isArray(arg: any): boolean {
   return Object.prototype.toString.call(arg) === '[object Array]';
 }
 
+function isBasic(arg:any): boolean {
+  return typeof arg === 'string' || typeof arg === 'number';
+}
+
 function mergeProps(object: any, source: any) {
   Object.keys(source).forEach((key) => {
-    if (typeof source[key] === 'string' || typeof source[key] === 'number') {
+    if (object[key] === undefined) {
       object[key] = source[key];
-    }
+    } else {
+      if (isBasic(source[key])) {
+        if (isBasic(object[key])) {
+          object[key] = source[key];
+        } else if (isArray(object[key])) {
+          object[key].push(source[key]);
+        }
+      }
 
-    if (isObject(source[key])) {
-      mergeProps(object[key], source[key]);
+      if (isObject(source[key])) {
+        if (isArray(object[key])) {
+          object[key].push(source[key]);
+        } else if (isObject(object[key])) {
+          mergeProps(object[key], source[key]);
+        } else {
+          object[key] = source[key];
+        }
+      }
+
+      if (isArray(source[key])) {
+        if (isArray(object[key])) {
+          object[key] = object[key].concat(source[key]);
+        } else if (isObject(object[key])) {
+          source[key].push(object[key]);
+          object[key] = source[key];
+        } else {
+          source[key].push(object[key]);
+          object[key] = source[key];
+        }
+      }
     }
   });
 }
