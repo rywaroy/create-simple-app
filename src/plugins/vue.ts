5366e9f99f2b67029798bd55e70b07d8d41542f5
rywaroy <464485682@qq.com>
2020-06-06 22:53:35 +0800

    feat: 添加vue插件

---
+++ src/plugins/vue.ts
@@ -0,0 +1,36 @@
+import GeneratorAPI from '../generator/generatorAPI';
+
+const vuePlugin = {
+  id: 'vue',
+  apply: (api: GeneratorAPI) => {
+    api.addModulePrompt({
+      name: 'Vue',
+      value: 'vue',
+    });
+    api.addPresetPromptCallBack(({ module }: { module: string[]}) => {
+      // 判断是否选择了vue
+      if (module.includes('vue')) {
+        api.chainWebpack()
+          .module
+          .rule('vue')
+          .test(/\.vue$/)
+          .use('vue-loader')
+          .loader('vue-loader');
+        api.extendPackage({
+          devDependencies: {
+            'vue-loader': '^15.9.2',
+            'vue-style-loader': '^4.1.2',
+            'vue-template-compiler': '^2.6.11',
+          },
+          dependencies: {
+            vue: '^2.6.11',
+            'vue-router': '^3.3.2',
+            vuex: '^3.4.0',
+          },
+        });
+      }
+    });
+  },
+};
+
+export default vuePlugin;
