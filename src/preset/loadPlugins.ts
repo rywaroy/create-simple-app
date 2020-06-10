c4fec1e263434007104f2f1e41f29170606e4973
rywaroy <464485682@qq.com>
2020-06-10 10:35:53 +0800

    fix

--- src/preset/loadPlugins.ts
+++ src/preset/loadPlugins.ts
@@ -6,7 +6,7 @@ import path = require('path');
 export default function loadPlugins(): IPlugin[] {
   const plugins: IPlugin[] = [];
   const pluginPath = glob.sync('./plugins/*/index.js', {
-    cwd: path.join(process.cwd(), 'lib'),
+    cwd: path.join(__dirname, '../../lib'),
   });
   pluginPath.forEach((item) => {
     const plugin: IPlugin = require(path.join('..', item)).default;
