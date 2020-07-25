787facf5d9da68e2613179df8193a24ba0082a09
rywaroy <464485682@qq.com>
2020-07-25 16:28:10 +0800

    feat: prettier interface

--- src/types/index.ts
+++ src/types/index.ts
@@ -6,3 +6,4 @@ export { IPackage } from './package';
 export { IGeneratorOtions } from './generator';
 export { IEslintConfig } from './eslint';
 export { IJestConfig } from './jest';
+export { IPrettierConfig } from './prettier';
---
+++ src/types/prettier.ts
@@ -0,0 +1,6 @@
+export interface IPrettierConfig {
+  singleQuote: boolean;
+  trailingComma: string;
+  printWidth: number;
+  [props: string]: any;
+}
