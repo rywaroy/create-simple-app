103f1e780e3333493dc8a892d95c262481983414
rywaroy <464485682@qq.com>
2020-08-07 15:41:12 +0800

    test: 测试不存在文件夹

--- test/add.test.ts
+++ test/add.test.ts
@@ -18,12 +18,12 @@ afterAll(() => {
   fs.removeSync(examplePath);
 });
 
-describe('测试create方法', () => {
+describe('测试add方法', () => {
   it('测试配置', async () => {
-    const generator = add('add-example', {
+    const generator = add('example', {
       module: ['commitlint', 'eslint', 'husky', 'jest', 'lint-staged', 'prettier', 'stylelint'],
     });
-    await generator.create();
+    await generator!.create();
     const pkg = fs.readJSONSync(path.join(examplePath, 'package.json'));
     expect(pkg.devDependencies).toHaveProperty('commitizen');
     expect(fs.existsSync(path.join(examplePath, '.eslintrc.js'))).toBeTruthy();
@@ -32,5 +32,12 @@ describe('测试create方法', () => {
     expect(pkg).toHaveProperty('lint-staged');
     expect(fs.existsSync(path.join(examplePath, '.prettierrc'))).toBeTruthy();
     expect(fs.existsSync(path.join(examplePath, '.stylelintrc'))).toBeTruthy();
+  });
+
+  it('测试不存在文件夹', async () => {
+    const generator = add('test', {
+      module: [],
+    });
+    expect(generator).toBeNull();
   });
 });
